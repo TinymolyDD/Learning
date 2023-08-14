260000557edbc7f61f36b37328b3f47e40607328
Tinymoly <zhujingjing110@gmail.com>
2023-08-13 22:40:34 -0700

    .

--- Kotlin/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Kotlin/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.lemonade
 
 import android.os.Bundle
-import android.support.v4.os.IResultReceiver.Default
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,9 +14,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -30,6 +27,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -52,54 +50,93 @@ class MainActivity : ComponentActivity() {
 fun LemonApp() {
     // A surface container using the 'background' color from the theme
     var currentStep by remember { mutableStateOf(1) }
+    var squeezeStep by remember { mutableStateOf(1) }
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        var imageResource = when(currentStep) {
-            1 -> R.drawable.lemon_tree
-            2 -> R.drawable.lemon_squeeze
-            3 -> R.drawable.lemon_drink
-            else -> R.drawable.lemon_restart
-        }
-        var textResource = when(currentStep) {
-            1 -> R.string.Tree
-            2 -> R.string.Lemon
-            3 -> R.string.Glass_of_lemonade
-            else -> R.string.Empty_glass
-        }
-        var content : String = when(currentStep) {
-            1 -> "lemon_tree"
-            2 -> "lemon_squeeze"
-            3 -> "lemon_drink"
-            else -> "lemon_restart"
-    }
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Box(
-                Modifier
-                    .size(240.dp, 240.dp)
-                    .clip(RoundedCornerShape(32.dp))
-                    .background(Color(0xFFCAEDB9))
-            ) {
-                Image(
-                    painter = painterResource(id = imageResource),
-                    contentDescription = content,
-                    modifier = Modifier
-                        .align(Alignment.Center)
-                        .clickable {
-                            currentStep = (currentStep + 1) % 4
-                        }
+        when(currentStep) {
+            1 -> {
+                LemonTextAndImage(
+                    painter = painterResource(id = R.drawable.lemon_tree),
+                    contentDescription = "lemon_tree",
+                    textContent = stringResource(id = R.string.Tree),
+                    onImageClicked = {
+                        currentStep = (currentStep + 1) % 4
+                        squeezeStep = (2..4).random()
+                    }
                 )
             }
-            Spacer(modifier = Modifier.height(32.dp))
-            Text(
-                stringResource(id = textResource),
-                fontSize = 18.sp
+            2 -> {
+                LemonTextAndImage(
+                    painter = painterResource(id = R.drawable.lemon_squeeze),
+                    contentDescription = "lemon_tree",
+                    textContent = stringResource(id = R.string.Lemon),
+                    onImageClicked = {
+                        when(squeezeStep) {
+                            in 2..4 -> squeezeStep--
+                            else -> currentStep =(currentStep + 1) % 4
+                        }
+                    }
+                )
+            }
+            3 -> {
+                LemonTextAndImage(
+                    painter = painterResource(id = R.drawable.lemon_drink),
+                    contentDescription = "lemon_drink",
+                    textContent = stringResource(id = R.string.Glass_of_lemonade),
+                    onImageClicked = {
+                        currentStep = (currentStep + 1) % 4
+                    }
+                )
+            }
+            0 -> {
+                LemonTextAndImage(
+                    painter = painterResource(id = R.drawable.lemon_restart),
+                    contentDescription = "lemon_restart",
+                    textContent = stringResource(id = R.string.Empty_glass),
+                    onImageClicked = {
+                        currentStep = (currentStep + 1) % 4
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun LemonTextAndImage(
+    painter: Painter,
+    contentDescription: String,
+    textContent: String,
+    onImageClicked: () -> Unit
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Box(
+            Modifier
+                .size(240.dp, 240.dp)
+                .border(2.dp, Color(185, 228, 160), RoundedCornerShape(32.dp) )
+                .clip(RoundedCornerShape(32.dp))
+                .background(Color(0xFFCAEDB9))
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .clickable {
+                        onImageClicked()
+                    }
             )
         }
+        Spacer(modifier = Modifier.height(32.dp))
+        Text(
+            textContent,
+            fontSize = 18.sp
+        )
     }
 }
 
